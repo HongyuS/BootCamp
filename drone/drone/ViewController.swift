7c66088682240d39fd7e420df81e2467c916353e
Hongyu Shi <118010258@link.cuhk.edu.cn>
2019-06-13 15:50:47 +0800

    Added new project: Drone Controller

---
+++ drone/drone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  drone
+//
+//  Created by Hongyu Shi on 2019/6/13.
+//  Copyright © 2019 HY S. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
