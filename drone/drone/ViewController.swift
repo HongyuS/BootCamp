54f7f5f405fdcf823fbe343e2681847d395de531
Hongyu Shi <118010258@link.cuhk.edu.cn>
2019-06-13 21:43:05 +0800

    fixed type converting problem by remove it

--- drone/drone/ViewController.swift
+++ drone/drone/ViewController.swift
@@ -13,8 +13,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        distanceSliderValue.text = "\(distanceSlider.value)"
+        rotateAngle.text = "\(rotateAngleSlider.value)"
     }
 
-
+    @IBOutlet weak var distanceSlider: UISlider!
+    @IBOutlet weak var distanceSliderValue: UILabel!
+    @IBAction func distanceSliderValueChange(_ sender: UISlider) {
+        distanceSliderValue.text = "\(sender.value)"
+    }
+    
+    @IBOutlet weak var rotateAngleSlider: UISlider!
+    @IBOutlet weak var rotateAngle: UILabel!
+    @IBAction func rotateAngleSliderValueChange(_ sender: UISlider) {
+        rotateAngle.text = "\(sender.value)"
+    }
+    
+    @IBOutlet weak var droneState: UILabel!
+    @IBOutlet weak var udpState: UILabel!
+    @IBOutlet weak var aliveState: UILabel!
+    
 }
 
