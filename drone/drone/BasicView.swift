7b118c41d2184297f74365677d7db7f9bea4c45a
Hongyu Shi <118010258@link.cuhk.edu.cn>
2019-06-20 12:41:13 +0800

    Add some outlets

--- drone/drone/BasicView.swift
+++ drone/drone/BasicView.swift
@@ -13,25 +13,44 @@ class BasicView: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Initialize label values of sliders.
         distanceSliderValue.text = "\(distanceSlider.value)"
         rotateAngle.text = "\(rotateAngleSlider.value)"
+        
     }
-
+    
+    // Motion Button outlet.
+    @IBOutlet weak var upButton: UIButton!
+    @IBOutlet weak var downButton: UIButton!
+    @IBOutlet weak var leftButton: UIButton!
+    @IBOutlet weak var rightButton: UIButton!
+    
+    // Master Button outlet.
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
+    // Distance Slider outlet and action to change value label.
     @IBOutlet weak var distanceSlider: UISlider!
     @IBOutlet weak var distanceSliderValue: UILabel!
     @IBAction func distanceSliderValueChange(_ sender: UISlider) {
         distanceSliderValue.text = "\(sender.value)"
     }
     
+    // Rotation Angle Slider outlet and action to change value label.
     @IBOutlet weak var rotateAngleSlider: UISlider!
     @IBOutlet weak var rotateAngle: UILabel!
     @IBAction func rotateAngleSliderValueChange(_ sender: UISlider) {
         rotateAngle.text = "\(sender.value)"
     }
     
-    @IBOutlet weak var droneState: UILabel!
-    @IBOutlet weak var udpState: UILabel!
-    @IBOutlet weak var aliveState: UILabel!
+    // Drone Status labels.
+    @IBOutlet weak var timeStatus: UILabel!
+    @IBOutlet weak var heightStatus: UILabel!
+    @IBOutlet weak var batteryStatus: UIProgressView!
+    @IBOutlet weak var droneStatus: UILabel!
+    @IBOutlet weak var connectionStatus: UILabel!
+    @IBOutlet weak var aliveStatus: UILabel!
     
 }
 
