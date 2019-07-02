5776bb4fbeda90e45dbd26c049b22f3ca2e9791f
Hongyu Shi <118010258@link.cuhk.edu.cn>
2019-07-02 23:10:32 +0800

    Add empty JoyStick View to the drone project

---
+++ drone/drone/JoyStickViewController.swift
@@ -0,0 +1,30 @@
+//
+//  JoyStickViewController.swift
+//  drone
+//
+//  Created by Hongyu Shi on 2019/7/2.
+//  Copyright © 2019 HY S. All rights reserved.
+//
+
+import UIKit
+
+class JoyStickViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
