a4eb5358f6cbaa3158200ac9027135485cc413c0
Hongyu Shi <44023928+HongyuS@users.noreply.github.com>
2020-06-19 14:58:29 +0800

    Regroup source code files; Add video streaming experiment

---
+++ drone-swiftUI/drone-swiftUI/DroneIO/VideoDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  VideoDelegate.swift
+//  drone-swiftUI
+//
+//  Created by Hongyu Shi on 2020/6/18.
+//  Copyright © 2020 Hongyu Shi. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+struct VideoDelegate {
+    var frames: [Data]
+}
